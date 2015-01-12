89a59ff47b83acdda6955771bc7bcfcfe85f7ded
herrernst <herr.ernst@gmail.com>
2015-01-12 23:08:01 +0100

    use regex to check

--- data/override-docwrite.js
+++ data/override-docwrite.js
@@ -1,9 +1,10 @@
 // document.documentElement.style.border = '1px solid red'
+var scriptSrcRegex = /<script\b[^<>]*\bsrc=/;
 function docWriteOverride(content) {
-  if (content.indexOf("<script") === -1) {
-    document.write(content)
-  } else {
+  if (scriptSrcRegex.test(content)) {
     console.log('skipping document.write() of: ' + content);
+  } else {
+    document.write(content)
   }
 }
 // unsafeWindow.document.write = docWriteOverride
